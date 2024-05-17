968d7f47bf8f387ee083b27d310694be063e7c60
Sanket Banerjee <sanketbanerjee.2004@gmail.com>
2024-05-17 16:21:26 +0530

    Production Ready

--- server.js
+++ server.js
@@ -1,44 +1,67 @@
-const { Socket } = require('socket.io')
-const mongoose = require('mongoose')
-const Document = require('./Document')
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const { Server } = require('socket.io');
+const Document = require('./Document');
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-mongoose.connect('mongodb+srv://sanketbanerjee:202004@cluster0.g1uple2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+// Set up Express
+const app = express();
 
-const io = require('socket.io')(3001, {
+// Apply global CORS settings
+app.use(cors({
+    origin: process.env.CLIENT_ORIGIN,
+    methods: ['GET', 'POST']
+}));
+
+// Create HTTP server
+const server = http.createServer(app);
+
+// Set up Socket.io with CORS settings
+const io = new Server(server, {
     cors: {
-        // origin: 'http://localhost:3000',
-        origin: 'https://docsme.netlify.app',
+        origin: process.env.CLIENT_ORIGIN,
         methods: ['GET', 'POST']
     },
-})
+});
 
 const defaultValue = "";
 
-io.on("connection", Socket => {
-
-    Socket.on('get-document', async documentId => {
-        const document = await findOrcreateDocument(documentId);
-        Socket.join(documentId)
-        Socket.emit("load-document", document.data)
-
-        Socket.on('send-changes', delta => {
-            Socket.broadcast.to(documentId).emit("receive-changes", delta)
-        })
-
-        Socket.on("save-document", async data => {
-            await Document.findByIdAndUpdate(documentId, { data });
-        })
-    })
+io.on("connection", (socket) => {
     console.log("Connected");
-})
 
+    socket.on('get-document', async (documentId) => {
+        const document = await findOrCreateDocument(documentId);
+        socket.join(documentId);
+        socket.emit("load-document", document.data);
 
-async function findOrcreateDocument(id) {
+        socket.on('send-changes', (delta) => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta);
+        });
+
+        socket.on("save-document", async (data) => {
+            await Document.findByIdAndUpdate(documentId, { data });
+        });
+    });
+});
+
+async function findOrCreateDocument(id) {
     if (id == null) return;
 
     const document = await Document.findById(id);
-
-    if (document) return document
-    return await Document.create({ _id: id, data: defaultValue })
+    if (document) return document;
+    return await Document.create({ _id: id, data: defaultValue });
 }
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
